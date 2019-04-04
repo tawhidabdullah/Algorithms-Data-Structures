721d7ef325c72f1a28c93a6f85833da730d36cd5
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-04-04 17:56:00 +0600

    slidingWindow and devide && conqre

--- PS-Patterns/frequencyCounter/frequencyCounter.js
+++ PS-Patterns/frequencyCounter/frequencyCounter.js
@@ -1,73 +1,82 @@
-// ====================================================================================================================================
+// // ====================================================================================================================================
 
-// problem :=>>
-// Write a function called same , which accepts 2 arrays, the function should return true, if every value in the array has i'ts 
-// corresponding value squared in the second array, the frequency of value must be same 
+// // problem :=>>
+// // Write a function called same , which accepts 2 arrays, the function should return true, if every value in the array has i'ts 
+// // corresponding value squared in the second array, the frequency of value must be same 
 
-// ====================================================================================================================================
-
-
-
-
-// inputs : 2 array , ([1,2,3],[1,4,6]); 
-// outputs : should return true because every array has it's corresponding squared value in the second value 
+// // ====================================================================================================================================
 
 
 
-function squared(arr1,arr2){
+
+// // inputs : 2 array , ([1,2,3],[1,4,6]); 
+// // outputs : should return true because every array has it's corresponding squared value in the second value 
+
+
+
+// function squared(arr1,arr2){
  
-    //check if the input is valid 
-    if(!(arr1.length === arr2.length)){
-        return false; 
-    } 
+//     //check if the input is valid 
+//     if(!(arr1.length === arr2.length)){
+//         return false; 
+//     } 
 
-    for (let i = 0; i< arr1.length; i++){
-        let currentIndex = arr2.indexOf(arr1[i]**2)
-        if(currentIndex === -1 ){
-            return false; 
-        }
-        arr2.splice(currentIndex,1); 
-    }
+//     for (let i = 0; i< arr1.length; i++){
+//         let currentIndex = arr2.indexOf(arr1[i]**2)
+//         if(currentIndex === -1 ){
+//             return false; 
+//         }
+//         arr2.splice(currentIndex,1); 
+//     }
 
-    return true; 
+//     return true; 
+// }; 
+
+// // console.log(squared([5,1,2,3,6],[1,9,4,25,6])); 
+
+
+
+// //////////////////////////      FREQUENCY COUNTER PATTERN / ALGORITHM        ///////////////////////////////
+
+
+// function frequencyCounter(arr1,arr2){
+//    if(arr1.length !== arr2.length){
+//        return false; 
+//    }; 
+
+//   const frequencyCounter1 = {}; 
+//   const frequencyCounter2 = {}; 
+
+//    for(let value of arr1){
+//     // so there is a value of arr1[value] return that value with plus 1 and if not return that a value with 1
+//        frequencyCounter1[value] = (frequencyCounter1[value] || 0) + 1; 
+//    }
+//    for(let value of arr2){
+//     // so there is a value of arr1[value] return that value with plus 1 and if not return that a value with 1
+//        frequencyCounter2[value] = (frequencyCounter2[value] || 0) + 1; 
+//    }
+
+//   for(let key in frequencyCounter1){
+//     if(!(key**2 in frequencyCounter2)){
+//         return false; 
+//     }
+//     if(frequencyCounter2[key**2] !== frequencyCounter1[key]){
+//         return false; 
+//     }
+//   }
+
+//   return true; 
+
+// }; 
+
+// console.log(frequencyCounter([5,1,2,3,6],[1,9,4,25,36]))
+
+let array = [234,3534564,353]; 
+
+for(let i = 0; i < array.length; i++){
+    console.log(array[i])
+    
 }; 
 
-// console.log(squared([5,1,2,3,6],[1,9,4,25,6])); 
 
-
-
-//////////////////////////      FREQUENCY COUNTER PATTERN / ALGORITHM        ///////////////////////////////
-
-
-function frequencyCounter(arr1,arr2){
-   if(arr1.length !== arr2.length){
-       return false; 
-   }; 
-
-  const frequencyCounter1 = {}; 
-  const frequencyCounter2 = {}; 
-
-   for(let value of arr1){
-    // so there is a value of arr1[value] return that value with plus 1 and if not return that a value with 1
-       frequencyCounter1[value] = (frequencyCounter1[value] || 0) + 1; 
-   }
-   for(let value of arr2){
-    // so there is a value of arr1[value] return that value with plus 1 and if not return that a value with 1
-       frequencyCounter2[value] = (frequencyCounter2[value] || 0) + 1; 
-   }
-
-  for(let key in frequencyCounter1){
-    if(!(key**2 in frequencyCounter2)){
-        return false; 
-    }
-    if(frequencyCounter2[key**2] !== frequencyCounter1[key]){
-        return false; 
-    }
-  }
-
-  return true; 
-
-}; 
-
-console.log(frequencyCounter([5,1,2,3,6],[1,9,4,25,36]))
-
+array.push(null); 
--- PS-Patterns/multiple-pointers/countUniquevalues.js
+++ PS-Patterns/multiple-pointers/countUniquevalues.js
@@ -1,40 +1,38 @@
 // ===============================================================================================================================
 
-    // implement a function a called countUnique values, which accept a sorted array, and count the uniquie values of the array, 
-    // There can be negitive number is the array, but it will sorted  
+// implement a function a called countUnique values, which accept a sorted array, and count the uniquie values of the array,
+// There can be negitive number is the array, but it will sorted
 
 // ===============================================================================================================================
 
-// input :  // sorted array  ==> [-2,-1,-1,0,1]; 
-// outputs : should return  ==> 4 
+// input :  // sorted array  ==> [-2,-1,-1,0,1];
+// outputs : should return  ==> 4
 
+function countUniqueValues(arr) {
+  // create 1 pointer from the beggining of the array
+  // NOTE: another pointer will created in the forloop
+  // for each element in the array from right next index of pointer 1 ===>> here we will create a another varibble
+  // if pointer one is not equal to pointer 2 then increment pointer 1
 
-function countUniqueValues(arr){
+  if (arr.length === 0) {
+    return "get a life";
+  }
 
-     // create 1 pointer from the beggining of the array 
-    // NOTE: another pointer will created in the forloop 
-        // for each element in the array from right next index of pointer 1 ===>> here we will create a another varibble 
-            // if pointer one is not equal to pointer 2 then increment pointer 1 
+  let pointerOne = 0;
+  let x = 0;
 
+  for (let pointerTwo = 1; pointerTwo < arr.length; pointerTwo++) {
+    // console.log(arr[pointerOne]);
+    if (arr[pointerOne] !== arr[pointerTwo]) {
+      x++;
+      arr[pointerOne] = arr[pointerTwo];
+      // console.log(arr[pointerOne]);
+    }
+  }
 
-            if(arr.length === 0){
-                return 'get a life'; 
-            }
+  return x + 1;
+}
 
-            let pointerOne = 0; 
+console.log(countUniqueValues([-2, -1, -1, -1, 0, 1, 3535, 3434, 5, 5]));
 
-            for(let pointerTwo = 1; pointerTwo < arr.length; pointerTwo++){
-                if(arr[pointerOne] !== arr[pointerTwo]){
-                    pointerOne++
-                    arr[pointerOne] = arr[pointerTwo]; 
-                }
-            }
-
-            return pointerOne+1
-
-}; 
-
-console.log(countUniqueValues([-2,-1,-1,0,1])); 
-
-
-
+// the array will absolutly has to be sorted
